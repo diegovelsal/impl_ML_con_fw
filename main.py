bd1527354192ce24ba59edc3682c79f54f7ae14a
diegovelsal <a01177877@itesm.mx>
2024-09-06 09:32:35 -0600

    cambio de datos y xgboost (overfitting).

--- main.py
+++ main.py
@@ -2,56 +2,138 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import xgboost as xgb
 
+# Función para modificar los datos para el entrenamiento del modelo
 def data_engineering(df):
-    # Drop colums street, country, date and statezip
-    df.drop(['street', 'country', 'date', 'statezip'], axis=1, inplace=True)
+    # Eliminación de columnas no necesarias
+    df = df.drop(['Unnamed: 0', 'comp', 'round', 'attendance', 'match report', 'notes', 'time'], axis=1)
 
-    # yr_renovated
-    df['yr_renovated'] = df.apply(lambda x: x['yr_built'] if x['yr_renovated'] < x['yr_built'] else x['yr_renovated'], axis=1)
-    df['yr_renovated'] = df.apply(lambda x: x['yr_built'] if x['yr_renovated'] == 0 else x['yr_renovated'], axis=1)
+    # Columnas que pudieran ser relevantes pero por el momento no se utilizarán
+    df = df.drop(['captain', 'formation', 'referee'], axis=1)
 
-    # Basement
-    bins = [0, 1, 500, 1000, 1500, 2000, float('inf')]
-    labels = [0, 1, 2, 3, 4, 5]
-    df['basement'] = pd.cut(df['sqft_basement'], bins=bins, labels=labels, right=False)
-    
-    # Nueva columna Seattle que es 1 si la ciudad es Seattle y 0 en otro caso
-    df['Seattle'] = df['city'].apply(lambda x: 1 if x == 'Seattle' else 0)
-    df.drop('city', axis=1, inplace=True)
+    # Cambio de tipo de datos para las columnas
+    df['date'] = pd.to_datetime(df['date'])
+    df['venue'] = df['venue'].astype('category')
+    df['opponent'] = df['opponent'].astype('category')
+    df['team'] = df['team'].astype('category')
+    df['result'] = df['result'].astype('category')
+
+    # Corregir variable 'day'
+    df['day'] = df['date'].dt.day_name()
+    df['day'] = df['day'].astype('category')
+
+    # Corregir variable 'season'
+    df.drop_duplicates(subset=df.columns.drop('season'), inplace=True)
+    df['season'] = pd.to_datetime(df['date']).apply(lambda date: date.year + 1 if date.month >= 8 else date.year)
+
+    # Eliminar columnas de date que ya no se utilizará
+    df = df.drop(['date'], axis=1)
+
+    # Eliminar donde la variable 'dist' sea nulos
+    # df = df.dropna(subset=['dist'])
+
+    # Para la variable 'Result', cambiarla a 1 si es victoria y a 0 si es empate o derrota
+    df['result'] = df['result'].apply(lambda result: 1 if result == 'W' else 0)
+    df['result'] = df['result'].astype('bool')
+
+    # Convertir variables categóricas a numéricas con get_dummies
+    df = pd.get_dummies(df, drop_first=False)
+    df = df.drop(['venue_Away'], axis=1)
+
+    print(df.info())
 
     return df
 
-def main():
-    # Carga de datos
-    # df = pd.read_csv('data/usa_housing.csv')
+# Función para separar los datos en entrenamiento, validación y prueba
+def split_data(df):
+    # Separar las variables independientes y dependientes
+    X = df.drop(['result'], axis=1)
+    y = df['result']
 
-    # Data Engineering para la preparación del modelo
-    # df = data_engineering(df)
-
-    # Save the data to a new csv file
-    # df.to_csv('data/usa_housing_engineered.csv', index=False)
-
-    # Carga de datos
-    df = pd.read_csv('data/usa_housing_engineered.csv')
-
-    # Definición de X y Y
-    X = df.drop('price', axis=1)
-    y = df['price']
-
-    # Dividir los datos en entrenamiento y prueba
+    # Separar los datos en entrenamiento (60%), validación (20%) y prueba (20%)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.16, random_state=42)
 
-    # Normalizar las características
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+# Función para escalar los datos
+def scale_data(X_train, X_val, X_test):
+    # Escalar los datos
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
+    X_val_scaled = scaler.transform(X_val)
     X_test_scaled = scaler.transform(X_test)
 
-    print("Datos preparados para el modelado")
+    return X_train_scaled, X_val_scaled, X_test_scaled
+
+# Función para entrenar y evaluar el modelo usando XGBoost
+def train_and_evaluate_model(X_train_scaled, X_val_scaled, y_train, y_val, X_test_scaled, y_test):
+    # Crear DMatrix para XGBoost
+    train_dmatrix = xgb.DMatrix(X_train_scaled, label=y_train)
+    val_dmatrix = xgb.DMatrix(X_val_scaled, label=y_val)
+    test_dmatrix = xgb.DMatrix(X_test_scaled, label=y_test)
+    
+    # Parámetros de XGBoost con regularización
+    params = {
+        'objective': 'binary:logistic',  # Clasificación binaria
+        'eval_metric': 'logloss',
+        'max_depth': 2,                  # Profundidad máxima de los árboles
+        'learning_rate': 0.003,            # Tasa de aprendizaje
+        'lambda': 0.1,                     # Regularización L2
+        'alpha': 0.1                   # Regularización L1
+    }
+
+    # Entrenar el modelo
+    model = xgb.train(params, train_dmatrix, num_boost_round=100, evals=[(val_dmatrix, 'eval')])
+
+    # Predecir en el conjunto de validación
+    y_val_pred_prob = model.predict(val_dmatrix)
+    y_val_pred = (y_val_pred_prob > 0.5).astype(int)
+    
+    # Evaluar en el conjunto de validación
+    accuracy_val = accuracy_score(y_val, y_val_pred)
+    confusion_val = confusion_matrix(y_val, y_val_pred)
+    report_val = classification_report(y_val, y_val_pred)
+
+    print('Validation Accuracy:', accuracy_val)
+    print('Validation Confusion Matrix:\n', confusion_val)
+    print('Validation Classification Report:\n', report_val)
+
+    # Predecir en el conjunto de prueba
+    y_test_pred_prob = model.predict(test_dmatrix)
+    y_test_pred = (y_test_pred_prob > 0.5).astype(int)
+
+    # Evaluar en el conjunto de prueba
+    accuracy_test = accuracy_score(y_test, y_test_pred)
+    confusion_test = confusion_matrix(y_test, y_test_pred)
+    report_test = classification_report(y_test, y_test_pred)
+
+    print('Test Accuracy:', accuracy_test)
+    print('Test Confusion Matrix:\n', confusion_test)
+    print('Test Classification Report:\n', report_test)
+
+def main():
+    # Carga de datos
+    df = pd.read_csv('data/matches.csv')
+
+    # Data Engineering
+    df = data_engineering(df)
+
+    # Almacenar datos en un archivo CSV
+    # df.to_csv('data/matches_clean.csv', index=False)
+
+    # Separar y escalar los datos
+    X_train, X_val, X_test, y_train, y_val, y_test = split_data(df)
+    X_train_scaled, X_val_scaled, X_test_scaled = scale_data(X_train, X_val, X_test)
+
+    # print(X_train.columns)
+    # print(y_train.head())
+    
+    # Entrenar y evaluar el modelo
+    train_and_evaluate_model(X_train_scaled, X_val_scaled, y_train, y_val, X_test_scaled, y_test)
+
 
 if __name__ == '__main__':
     main()
-
-'''
-['Seattle', 'Carnation', 'Issaquah', 'Maple Valley', 'Kent', 'Redmond', 'Clyde Hill', 'Shoreline', 'Mercer Island', 'Auburn', 'Bellevue', 'Duvall', 'Renton', 'Sammamish', 'Woodinville', 'Kirkland', 'Burien', 'Federal Way', 'Normandy Park', 'Vashon', 'Kenmore', 'Yarrow Point', 'SeaTac', 'Newcastle', 'Covington', 'Lake Forest Park', 'Snoqualmie', 'Des Moines', 'Fall City', 'Ravensdale', 'Tukwila', 'North Bend', 'Medina', 'Bothell', 'Enumclaw','Snoqualmie Pass', 'Pacific', 'Black Diamond', 'Beaux Arts Village', 'Algona','Preston', 'Milton', 'Skykomish']
-'''
